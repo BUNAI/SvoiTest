3ec9e7805874b485a19325296230a76c74cb55c3
Bundovich <ai.bundovich@yandex.ru>
2023-03-12 02:04:42 +0700

    Замена int на long

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int [] manager = {2,3,4};
+        long [] manager = {2,3,4};
 
         SalesManager salesManager= new SalesManager(manager);
         System.out.println(salesManager.max());
