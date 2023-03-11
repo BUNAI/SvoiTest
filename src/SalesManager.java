57819140c3d1b353f96a03ac6bf47e3c65d4abb9
Bundovich <ai.bundovich@yandex.ru>
2023-03-12 02:31:17 +0700

    Добавил данные в ветку stat

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -16,5 +16,23 @@ public class SalesManager {
         return max;
     }
 
-}
+    public int min (){
+        int minValue = sales[0];
+        for(int i=1;i<sales.length;i++){
+            if(sales[i] < minValue){
+                minValue = sales[i];
+            }
+        }
+        return minValue;
+    }
 
+    public  int average() {
+        int total = 0;
+        int avg;
+        for(int i=0; i<sales.length; i++){
+            total = total + sales[i];
+        }
+        return avg = (total-max()-min()) / (sales.length-2);
+    }
+
+}
