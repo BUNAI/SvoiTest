244b6131ef89eaff8dae824e7ac8ad386d89dbab
Bundovich <ai.bundovich@yandex.ru>
2023-03-12 20:48:03 +0700

    Добавил решение конфликта

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int [] manager = {24,6,44,4};
+        long [] manager = {24,6,44,4};
+
 
         SalesManager salesManager= new SalesManager(manager);
         System.out.println(salesManager.average());
+        System.out.println(salesManager.max());
 
     }
 }
