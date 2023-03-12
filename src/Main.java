daafa2de43519117f47aae9c4fb2435b90d28697
Bundovich <ai.bundovich@yandex.ru>
2023-03-12 20:53:35 +0700

    Тестово внес изменения2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int [] manager = {24,6,44,4};
+        int [] manager = {434,24,6,44,4};
 
         SalesManager salesManager= new SalesManager(manager);
         System.out.println(salesManager.average());
